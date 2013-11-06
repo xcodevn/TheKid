0edf5a8267e61679140af0d605f0d78f7f7286ee
Tho^ng Nguyen <xcodevn@gmail.com>
2013-11-06 14:12:56 +0100

    A workaround for using old plugin webjars with play 2.2

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.webjars" %% "webjars-play" % "2.1.0-3",
+    "org.webjars" %% "webjars-play" % "2.1.0-3"  exclude("org.scala-stm", "scala-stm_2.10.0") exclude("play", "*"), 
     "org.webjars" % "bootstrap" % "2.1.1"
   )
 
